3a204fcd4b5b1cc1a311db7f9a0751eb73e1981a
Chris DeSilva <chris.a.desilva@gmail.com>
2019-10-15 09:21:55 -0500

    Add redirects plugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,8 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage, createRedirect } = actions
+ 
+    createRedirect({ fromPath: 'https://insightdatascience.skills.fund/*', toPath: 'https://insight.skills.fund/:splat', isPermanent: true, force: true })
+    createRedirect({ fromPath: '/go', toPath: '/?utm_source=school&utm_medium=printedskfbrochure&utm_campaign=studentbrochure'})
+    createRedirect({ fromPath: '/accepted', toPath: '/'})
+   
+}
