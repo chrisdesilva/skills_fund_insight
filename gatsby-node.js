0d5f54e28950be887de86cba390552b88474a23e
Arthur Kang <akang@goalsolutions.com>
2021-01-14 00:50:41 -0800

    Redirect Site To Main Skills Fund Domain

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,24 @@
 exports.createPages = ({ graphql, actions }) => {
     const { createPage, createRedirect } = actions
- 
-    createRedirect({ fromPath: 'https://insightdatascience.skills.fund/*', toPath: 'https://insight.skills.fund/:splat', isPermanent: true, force: true })
-    createRedirect({ fromPath: '/go', toPath: '/?utm_source=school&utm_medium=printedskfbrochure&utm_campaign=studentbrochure'})
-    createRedirect({ fromPath: '/accepted', toPath: '/'})
-   
+
+    createRedirect({
+        fromPath: 'https://insightdatascience.skills.fund/*',
+        toPath: 'https://skills.fund',
+        isPermanent: true,
+        force: true
+    })
+    createRedirect({
+        fromPath: 'https://insight.skills.fund/*',
+        toPath: 'hthttps://skills.fund',
+        isPermanent: true,
+        force: true
+    })
+    createRedirect({
+        fromPath: '/go',
+        toPath: '/?utm_source=school&utm_medium=printedskfbrochure&utm_campaign=studentbrochure'
+    })
+    createRedirect({
+        fromPath: '/accepted',
+        toPath: '/'
+    })
 }
